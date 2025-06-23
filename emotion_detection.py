6f8c0537ab213187e34d46d10a6d6a72efa8aabe
KrazyKirby2424 <ajdowd@gmail.com>
2025-06-23 18:20:48 -0400

    modified emotion_detector logic and output format

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,24 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=header)
-    return response.text
+
+    formatted_response = json.loads(response.text)
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+
+    output = "{\n"
+
+    for key, value in emotions.items():
+        output += f"'{key}': {value},\n"
+
+    #anger = formatted_response['emotionPredictions'][0]['emotion']['anger']
+
+    dominant_emotion = max(emotions, key=emotions.get)
+    output += "'dominant_emotion': '" + dominant_emotion + "'"
+    output += "\n}"
+
+    return print(output)
